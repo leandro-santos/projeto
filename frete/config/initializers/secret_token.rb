ceb9045f73946e4969828d7b8d4632d4eeec917f
Leandro <leandrosantos@clickjogos.com.br>
2012-11-13 09:09:17 -0200

    atualizacao 13/11/12

---
+++ frete/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Frete::Application.config.secret_token = 'e48f3b3167ee691e1c7f3943de17114017ed46e2018d4578a59b66e6fe74809fb872e55a9708117c9d3e0312cc69b89c2590fd52c09e04a4b8055ab31834b24d'
